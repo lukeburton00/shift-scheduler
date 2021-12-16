2721630584b3708dd2e05295fb8581a655d4c6fd
devluke00 <lukesburton19@gmail.com>
2021-12-15 18:47:52 -0700

    employee phone number validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,6 @@
 class Employee < ApplicationRecord
     belongs_to :user
     has_many :busies, dependent: :destroy
+
+    validates :phone, format: {with: /\d\d\d-\d\d\d-\d\d\d\d/, message: "Invalid phone number. Format is 000-000-0000"}
 end
